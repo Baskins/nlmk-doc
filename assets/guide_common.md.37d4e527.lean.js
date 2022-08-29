22c45cee12bd8490b8c11dc390727b3220263a9a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-08-29 08:29:11 +0000

    Deploy Baskins/nlmk-doc to github.com/Baskins/nlmk-doc.git:gh-pages

---
+++ assets/guide_common.md.37d4e527.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as r,d as a}from"./app.22e59867.js";const l='{"title":"Общее знакомство","description":"","frontmatter":{},"headers":[{"level":2,"title":"Обзорные видео","slug":"обзорные-видео"}],"relativePath":"guide/common.md","lastUpdated":1661761719660}',t={},i=a('',7);t.render=function(a,l,t,o,n,h){return e(),r("div",null,[i])};export default t;export{l as __pageData};
